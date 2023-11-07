e1f885dad44352082159f2ec23bc1ca144d1586e
yankee <yankeesuprem@gmail.com>
2023-11-07 09:09:15 +0000

    website down

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -28,11 +28,86 @@
 </head>
 
 <body class=" bg-white position-relative">
+  <main>
 
-  <main class="hero" style="background-image: url(<?= base_url('trucks.svg') ?>);">
-    <div class="overlay">
-      yankee
-    </div>
+    <section class="hero" style="background-image: url(<?= base_url('poly-trucks.png') ?>);">
+      <div class="overlay text-white d-flex flex-column align-items-center justify-content-center p-3">
+
+        <div class="container">
+          <div class="display-4 fw-bold text-center mb-3">Page en maintenance</div>
+          <div class="d-flex flex-column flex-md-row gap-2 justify-content-center">
+            <div class="text-white d-flex align-items-center gap-2 border rounded-3 p-3 justify-content-center">
+              <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+              </svg>
+              77 650 02 96
+            </div>
+            <div class="text-white d-flex align-items-center gap-2 border rounded-3 p-3 justify-content-center">
+              <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+              </svg>
+              33 842 52 58
+            </div>
+            <div class="text-white d-flex align-items-center gap-2 border rounded-3 p-3 justify-content-center">
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M14 3H2a1 1 0 00-1 1v8a1 1 0 001 1h12a1 1 0 001-1V4a1 1 0 00-1-1zM2 2a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V4a2 2 0 00-2-2H2z" clip-rule="evenodd"></path>
+                <path fill-rule="evenodd" d="M.071 4.243a.5.5 0 01.686-.172L8 8.417l7.243-4.346a.5.5 0 01.514.858L8 9.583.243 4.93a.5.5 0 01-.172-.686z" clip-rule="evenodd"></path>
+                <path d="M6.752 8.932l.432-.252-.504-.864-.432.252.504.864zm-6 3.5l6-3.5-.504-.864-6 3.5.504.864zm8.496-3.5l-.432-.252.504-.864.432.252-.504.864zm6 3.5l-6-3.5.504-.864 6 3.5-.504.864z"></path>
+              </svg>
+              contact@poly-trans.sn
+            </div>
+          </div>
+        </div>
+
+        <!-- <div class="container">
+          <h1 class="text-white display-6 fw-bold text">Entreprise de Transit, Transport, Manutention, et Logistique au Sénégal</h1>
+          <p class="fs-1">POLY-TRANS SUARL - La discipline la compétence et la rigeur est notre savoir faire.</p>
+          <div class="d-grid gap-3 d-md-flex pb-5">
+            <button class="btn btn-primary btn-lg">Nos Services</button>
+            <button class="btn btn-outline-primary btn-lg">À propos de nous</button>
+          </div>
+        </div> -->
+      </div>
+    </section>
+    <!-- <section>
+      <div class="container p-3">
+        <h2 class="fs-1 text-primary my-5">
+          Découvrez nos services
+        </h2>
+        <div class="row">
+          <div class="col-md-6 col-lg-4 d-flex flex-column gap-2 mb-4 mb-md-0">
+            <img src="<?= base_url('dpw.jpeg') ?>" alt="Port de Dakar - Dubai Port" class="serv rounded mb-2">
+            <h3 class="fw-bold text-primary">Transit international et national</h3>
+            <p>Facilitez vos échanges internationaux avec notre expertise en transit global.</p>
+          </div>
+          <div class="col-md-6 col-lg-4 d-flex flex-column gap-2 mb-4 mb-md-0">
+            <img src="<?= base_url('poly-trucks.png') ?>" alt="Flotte tracteurs de POLY-TRANS SUARL" class="serv rounded mb-2">
+            <h3 class="fw-bold text-primary">Transport Fiable</h3>
+            <p>Transport de marchandises sécurisé et ponctuel à l'échelle nationale.</p>
+          </div>
+          <div class="col-md-6 col-lg-4 d-flex flex-column gap-2 mb-4 mb-md-0">
+            <img src="<?= base_url('dpw.jpeg') ?>" alt="Transit international et national" class="serv rounded mb-2">
+            <h3 class="fw-bold text-primary">Transit international et national</h3>
+            <p>Facilitez vos échanges internationaux avec notre expertise en transit global.</p>
+          </div>
+          <div class="col-md-6 col-lg-4 d-flex flex-column gap-2 mb-4 mb-md-0">
+            <img src="<?= base_url('dpw.jpeg') ?>" alt="Transit international et national" class="serv rounded mb-2">
+            <h3 class="fw-bold text-primary">Transit international et national</h3>
+            <p>Facilitez vos échanges internationaux avec notre expertise en transit global.</p>
+          </div>
+          <div class="col-md-6 col-lg-4 d-flex flex-column gap-2 mb-4 mb-md-0">
+            <img src="<?= base_url('dpw.jpeg') ?>" alt="Transit international et national" class="serv rounded mb-2">
+            <h3 class="fw-bold text-primary">Transit international et national</h3>
+            <p>Facilitez vos échanges internationaux avec notre expertise en transit global.</p>
+          </div>
+          <div class="col-md-6 col-lg-4 d-flex flex-column gap-2 mb-4 mb-md-0">
+            <img src="<?= base_url('dpw.jpeg') ?>" alt="Transit international et national" class="serv rounded mb-2">
+            <h3 class="fw-bold text-primary">Transit international et national</h3>
+            <p>Facilitez vos échanges internationaux avec notre expertise en transit global.</p>
+          </div>
+        </div>
+      </div>
+    </section> -->
   </main>
 
 
