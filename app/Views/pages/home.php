bad338148f22ff17b9b604a6784880489d25f27c
yankee <yankeesuprem@gmail.com>
2023-11-08 11:43:14 +0000

    Htaccess + editing

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -15,7 +15,7 @@
   <meta property="og:description" content="Offrant des services de transit, transport, manutention, consignation, entreposage, groupage et conseils sur mesure depuis 2017." />
   <meta property="og:image" content="<?= base_url('poly-trans.png') ?>" />
   <meta property="og:url" content="<?= base_url() ?>" />
-  <meta property="og:type" content="website"/>
+  <meta property="og:type" content="website" />
 
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -28,34 +28,36 @@
 </head>
 
 <body class="position-relative">
+  <?= view('pages/components/header', [
+    'p' => 'home'
+  ]) ?>
   <main>
 
     <section class="hero" style="background-image: url(<?= base_url('poly-trucks.png') ?>);">
-      <div class="overlay text-white d-flex flex-column align-items-center justify-content-center p-3">
-
+      <div class="container-fluid overlay text-white d-flex flex-column align-items-center justify-content-center p-3">
         <div class="container">
-          <div class="display-4 fw-bold text-center mb-3">Page en maintenance</div>
+          <div class="display-5 fw-bold text-center mb-3">Page en maintenance</div>
           <div class="d-flex flex-column flex-md-row gap-2 justify-content-center mb-3">
-            <div class="text-white d-flex align-items-center gap-2 border rounded-3 p-3 justify-content-center">
+            <a href="tel:(221)776500296" class="text-white d-flex align-items-center gap-2 border rounded-3 p-3 justify-content-center">
               <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
               </svg>
               77 650 02 96
-            </div>
-            <div class="text-white d-flex align-items-center gap-2 border rounded-3 p-3 justify-content-center">
+            </a>
+            <a href="tel:(221)338425258" class="text-white d-flex align-items-center gap-2 border rounded-3 p-3 justify-content-center">
               <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
               </svg>
               33 842 52 58
-            </div>
-            <div class="text-white d-flex align-items-center gap-2 border rounded-3 p-3 justify-content-center">
+            </a>
+            <a href="mailto:contact@poly-trans.sn" class="text-white d-flex align-items-center gap-2 border rounded-3 p-3 justify-content-center">
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M14 3H2a1 1 0 00-1 1v8a1 1 0 001 1h12a1 1 0 001-1V4a1 1 0 00-1-1zM2 2a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V4a2 2 0 00-2-2H2z" clip-rule="evenodd"></path>
                 <path fill-rule="evenodd" d="M.071 4.243a.5.5 0 01.686-.172L8 8.417l7.243-4.346a.5.5 0 01.514.858L8 9.583.243 4.93a.5.5 0 01-.172-.686z" clip-rule="evenodd"></path>
                 <path d="M6.752 8.932l.432-.252-.504-.864-.432.252.504.864zm-6 3.5l6-3.5-.504-.864-6 3.5.504.864zm8.496-3.5l-.432-.252.504-.864.432.252-.504.864zm6 3.5l-6-3.5.504-.864 6 3.5-.504.864z"></path>
               </svg>
               contact@poly-trans.sn
-            </div>
+            </a>
           </div>
           <a class="btn btn-primary d-flex gap-2 btn-lg py-3 justify-content-center align-items-center text-white m-auto" style="width: max-content" href="/tracking" role="button">
             <svg stroke="#FFFFFF" fill="#FFFFFF" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
--- app/Views/pages/tracking.php
+++ app/Views/pages/tracking.php
@@ -156,7 +156,7 @@
   <script>
     const url = '<?= base_url() ?>';
   </script>
-  <script src="<?= base_url('tracking.js') ?>"></script>
+  <script src="<?= base_url('trackingModule.js') ?>"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
